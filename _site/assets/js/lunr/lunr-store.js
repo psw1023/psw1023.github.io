acafc91b1eff3889aae21f3b17e6ddc70f2ebc8b
Park Sunwoo <psw1023@unist.ac.kr>
2025-04-11 18:47:13 +0900

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,4 @@
 var store = [{
-        "title": "TEST1",
-        "excerpt":"안녕하세요.  ","categories": ["Theory"],
-        "tags": [],
-        "url": "/categories/theory/test1/",
-        "teaser": null
-      },{
-        "title": "TEST2",
-        "excerpt":"안녕하세요.  ","categories": ["Paper Review"],
-        "tags": [],
-        "url": "/categories/paper-review/test2/",
-        "teaser": null
-      },{
         "title": "[cs231n] 1.Image Classfication",
         "excerpt":"본 글은 Stanford University에서 2017년에 진행했던 cs231n 강의를 듣고 내용을 정리한 것입니다. 이번 글에서는 1강과 2강의 내용을 요약 및 정리하였습니다.   * block 처리된 문구를 클릭하면 해당 회차의 유튜브 영상으로 바로 갑니다. 강의가 2017년도 기준으로 제작되어 가끔씩 요즘 CV분야의 유행과는 괴리감드는 내용이 있을 수 있으나, 수강 목적이 글쓴이가 기초를 다지기...","categories": ["Theory"],
         "tags": [],
